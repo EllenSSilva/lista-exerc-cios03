3391dfdc767f88f599fe37a547d14c89a0fc3dcc
Ellen Silva <137661449+EllenSSilva@users.noreply.github.com>
2024-04-12 23:41:17 -0300

    Salvar

--- rotas/arquivos.js
+++ rotas/arquivos.js
@@ -1,30 +1,22 @@
-//importa express
 const express = require('express')
 
-//criar um router
 const router = express.Router()
 
-//dados Mockados
-
  let listaPessoas =[]
-// read busca todas as pessoas
  router.get('/pessoas', (req, res)=>{
     res.json(listaPessoas)
 })
-// read busca contato pelo 
 router.get('/pessoas/:id',(req,res)=>{
     const id = req.params.id
     const pessoas = listaPessoas[id]
     res.json(pessoas)
 })
 
-//read cadastra contatos
 router.post('/pessoas', (req, res)=>{
     const pessoas = req.body
     listaPessoas.push(pessoas)
     res.json("Pessoa cadastrado ")
 })
-//read deleta contato pelo id 
 router.delete('/pessoas/:id',(req, res)=>{
     const id = req.params.id
 listaPessoas.splice(id, 1)
@@ -32,7 +24,6 @@ res.json("Pessoa deletada")
 
 })
 
-//upadate atualização contatos
 router.put('/pessoas/:id', (req, res )=>{
     const id = req.params.id
     const {nome, idade, email,telefone} = req.body
@@ -40,6 +31,4 @@ router.put('/pessoas/:id', (req, res )=>{
     listaPessoas[id]= pessoaAtualizada
         res.json(pessoaAtualizada)
     })
-
-//exporrta router
 module.exports = router
